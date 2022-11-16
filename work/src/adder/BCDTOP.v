5f89665598836439f9cb34a3bac7456c7ed597ab
hirokiwa <hirokifsys@gmail.com>
2022-11-16 15:29:22 +0900

    edit BCDTOP & hosei6

--- work/src/adder/BCDTOP.v
+++ work/src/adder/BCDTOP.v
@@ -8,20 +8,6 @@ module BCDTOP(X, Y, S, CARRY, dummy);
     carrydetect carrydetect(f1, f3, f2, f3, cry);
     hosei6 hosei6(f1, f2, cry, f3, S[3:1], dummy);
 
-    S[0] = f0;
-
-    initial begin
-                X = 4'h0; Y = 4'h0;
-        #50     X = 4'h3; Y = 4'h5;
-        #50     X = 4'h4; Y = 4'h6;
-        #50     X = 4'h9; Y = 4'h9;
-        #50     X = 4'h7; Y = 4'h6;
-        #50     X = 4'h1; Y = 4'h8;
-        #50     $finish;
-    end
-
-    initial begin
-        $monitor("%t %h %h %h %h" , $time, a, b, sum, carry );
-    end
+    assign S[0] = f0;
 
 endmodule
--- work/src/adder/hosei6.v
+++ work/src/adder/hosei6.v
@@ -4,8 +4,8 @@ output [2:0] ss;
 output dum;
 wire d1, d2;
 
-halfadder HA1(h1, v1, ss[0], d1);
-fulladder FA4(A[1], B[1], d1, ss[1], d2);
+halfadder HA1(hs1, v1, ss[0], d1);
+fulladder FA4(hs2, v1, d1, ss[1], d2);
 halfadder HA2(d2, hs3, ss[2], dum);
 
 endmodule
